c4ffeaf1eddb2309fc551c9981db71fa6af7170b
Mohamed sobhy <mohamedyamany@055@gmail.com>
2023-10-25 14:56:32 +0300

    gentrate route slide animation

--- lib/core/routes/route_setting.dart
+++ lib/core/routes/route_setting.dart
@@ -21,38 +21,159 @@ class AppRouting {
   static Route? genertateRoute(RouteSettings settings) {
     switch (settings.name) {
       case RoutesName.spalsh:
-        return MaterialPageRoute(builder: (_) => const SplashScreen());
+        return PageRouteBuilder(
+          pageBuilder: (_, __, ___) => const SplashScreen(),
+          transitionDuration: const Duration(milliseconds: 400),
+          transitionsBuilder: (_, animation, __, child) {
+            return SlideTransition(
+              position: Tween<Offset>(
+                begin: const Offset(1, 0),
+                end: Offset.zero,
+              ).animate(animation),
+              child: child,
+            );
+          },
+        );
       case RoutesName.login:
-        return MaterialPageRoute(builder: (_) => const LoginScreen());
+        return PageRouteBuilder(
+          pageBuilder: (_, __, ___) => const LoginScreen(),
+          transitionDuration: const Duration(milliseconds: 400),
+          transitionsBuilder: (_, animation, __, child) {
+            return SlideTransition(
+              position: Tween<Offset>(
+                begin: const Offset(1, 0),
+                end: Offset.zero,
+              ).animate(animation),
+              child: child,
+            );
+          },
+        );
       case RoutesName.signUp:
-        return MaterialPageRoute(builder: (_) => const SignUpScreeen());
+        PageRouteBuilder(
+          pageBuilder: (_, __, ___) => const SignUpScreeen(),
+          transitionDuration: const Duration(milliseconds: 400),
+          transitionsBuilder: (_, animation, __, child) {
+            return SlideTransition(
+              position: Tween<Offset>(
+                begin: const Offset(1, 0),
+                end: Offset.zero,
+              ).animate(animation),
+              child: child,
+            );
+          },
+        );
       case RoutesName.boardingScreen:
-        return MaterialPageRoute(builder: (_) => const OnbordingScreen());
+        return PageRouteBuilder(
+          pageBuilder: (_, __, ___) => const OnbordingScreen(),
+          transitionDuration: const Duration(milliseconds: 400),
+          transitionsBuilder: (_, animation, __, child) {
+            return SlideTransition(
+              position: Tween<Offset>(
+                begin: const Offset(1, 0),
+                end: Offset.zero,
+              ).animate(animation),
+              child: child,
+            );
+          },
+        );
       case RoutesName.ottp:
-        return MaterialPageRoute(builder: (_) => OttpScreen());
+        PageRouteBuilder(
+          pageBuilder: (_, __, ___) => OttpScreen(),
+          transitionDuration: const Duration(milliseconds: 400),
+          transitionsBuilder: (_, animation, __, child) {
+            return SlideTransition(
+              position: Tween<Offset>(
+                begin: const Offset(1, 0),
+                end: Offset.zero,
+              ).animate(animation),
+              child: child,
+            );
+          },
+        );
       case RoutesName.bookDetails:
         Book book = settings.arguments as Book;
-        return MaterialPageRoute(
-          builder: (_) => BookDetailsScreen(book: book),
+        PageRouteBuilder(
+          pageBuilder: (_, __, ___) => BookDetailsScreen(
+            book: book,
+          ),
+          transitionDuration: const Duration(milliseconds: 400),
+          transitionsBuilder: (_, animation, __, child) {
+            return SlideTransition(
+              position: Tween<Offset>(
+                begin: const Offset(1, 0),
+                end: Offset.zero,
+              ).animate(animation),
+              child: child,
+            );
+          },
         );
       case RoutesName.homelayout:
-        return MaterialPageRoute(builder: (_) => const HomeLayout());
+        return PageRouteBuilder(
+          pageBuilder: (_, __, ___) => const HomeLayout(),
+          transitionDuration: const Duration(milliseconds: 400),
+          transitionsBuilder: (_, animation, __, child) {
+            return SlideTransition(
+              position: Tween<Offset>(
+                begin: const Offset(1, 0),
+                end: Offset.zero,
+              ).animate(animation),
+              child: child,
+            );
+          },
+        );
       case RoutesName.updatePassword:
-        return MaterialPageRoute(builder: (_) => const UpdatePasswordScreen());
+        PageRouteBuilder(
+          pageBuilder: (_, __, ___) => const UpdatePasswordScreen(),
+          transitionDuration: const Duration(milliseconds: 400),
+          transitionsBuilder: (_, animation, __, child) {
+            return SlideTransition(
+              position: Tween<Offset>(
+                begin: const Offset(1, 0),
+                end: Offset.zero,
+              ).animate(animation),
+              child: child,
+            );
+          },
+        );
       case RoutesName.userProfile:
         return MaterialPageRoute(builder: (_) => const UserProfileScreen());
       case RoutesName.sendOrder:
         return MaterialPageRoute(builder: (_) => const OrderScreen());
       case RoutesName.contactUs:
-        return MaterialPageRoute(builder: (_) => const ContactUsScreen());
+        return PageRouteBuilder(
+          pageBuilder: (_, __, ___) => const ContactUsScreen(),
+          transitionDuration: const Duration(milliseconds: 400),
+          transitionsBuilder: (_, animation, __, child) {
+            return SlideTransition(
+              position: Tween<Offset>(
+                begin: const Offset(1, 0),
+                end: Offset.zero,
+              ).animate(animation),
+              child: child,
+            );
+          },
+        );
       case RoutesName.resetPassword:
         return MaterialPageRoute(builder: (_) => const ForgetPassWordScreen());
       case RoutesName.orderHistory:
-        return MaterialPageRoute(builder: (_) => const OrderHistoryScreen());
+        PageRouteBuilder(
+          pageBuilder: (_, __, ___) => const OrderHistoryScreen(),
+          transitionDuration: const Duration(milliseconds: 400),
+          transitionsBuilder: (_, animation, __, child) {
+            return SlideTransition(
+              position: Tween<Offset>(
+                begin: const Offset(1, 0),
+                end: Offset.zero,
+              ).animate(animation),
+              child: child,
+            );
+          },
+        );
       case RoutesName.category:
         Category category = settings.arguments as Category;
         return MaterialPageRoute(
-            builder: (_) => CategoryScreen(category: category));
+          builder: (_) => CategoryScreen(category: category),
+        );
     }
 
     return null;
