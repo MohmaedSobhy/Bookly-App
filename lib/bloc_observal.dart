89540a37b7b5aaaaeea5d26ea128ebfb33ecfc33
Mohamed sobhy <mohamedyamany@055@gmail.com>
2023-10-27 13:20:16 +0200

    pub yaml package

--- lib/bloc_observal.dart
+++ lib/bloc_observal.dart
@@ -14,7 +14,6 @@ class SimpleBlocObserval extends BlocObserver {
   @override
   void onChange(BlocBase bloc, Change change) {
     super.onChange(bloc, change);
-    print(change);
   }
 
   @override
