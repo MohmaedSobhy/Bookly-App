de2f0b692633562daec3e029a05be62a76aa2126
Mohamed sobhy <mohamedyamany@055@gmail.com>
2023-10-29 11:25:44 +0200

    handle internet connection in cart screen

--- lib/feature/cart/screens/cart_screen.dart
+++ lib/feature/cart/screens/cart_screen.dart
@@ -3,6 +3,8 @@ import 'package:books_app/feature/cart/controller/cart_state.dart';
 import 'package:books_app/feature/cart/views/empty_cart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_offline/flutter_offline.dart';
+import '../../../core/widgets/no_internet_conection.dart';
 import '../views/cart_list_items.dart';
 import '../views/loading_cart_items.dart';
 
@@ -21,21 +23,38 @@ class CartScreen extends StatelessWidget {
               right: MediaQuery.sizeOf(context).width * 0.02,
               top: MediaQuery.sizeOf(context).height * 0.05,
             ),
-            child: BlocConsumer<CartCubit, CartState>(
-              listener: (context, state) {},
-              builder: (context, state) {
-                if (state is LoadingCartItems) {
-                  return const LoadingCartItemsView();
-                }
-                if (CartCubit.getInstanse().books.isEmpty) {
-                  return const EmptyCartView();
-                }
-                return const CartListItemsView();
+            child: OfflineBuilder(
+              connectivityBuilder: (BuildContext context,
+                  ConnectivityResult connectivity, Widget child) {
+                return (connectivity != ConnectivityResult.none)
+                    ? const CartView()
+                    : const NoInternetConnectionWidget();
               },
+              child: const CartView(),
             ),
           ),
         ),
       ),
+    );
+  }
+}
+
+class CartView extends StatelessWidget {
+  const CartView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<CartCubit, CartState>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        if (state is LoadingCartItems) {
+          return const LoadingCartItemsView();
+        }
+        if (CartCubit.getInstanse().books.isEmpty) {
+          return const EmptyCartView();
+        }
+        return const CartListItemsView();
+      },
     );
   }
 }
