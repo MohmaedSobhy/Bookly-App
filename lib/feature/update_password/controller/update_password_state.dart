4c23f33745a832fb688f0778f6ddac3f9c5c0570
Mohamed sobhy <mohamedyamany@055@gmail.com>
2023-10-29 12:36:09 +0200

    delete uncessary state from update password state

--- lib/feature/update_password/controller/update_password_state.dart
+++ lib/feature/update_password/controller/update_password_state.dart
@@ -5,5 +5,3 @@ class UpdatePasswordInitialState extends UpdatePasswordState {}
 class UpdatePasswordSuccefullyState extends UpdatePasswordState {}
 
 class UpdatePasswordFailedState extends UpdatePasswordState {}
-
-class ChangeVisiblityIcon extends UpdatePasswordState {}
